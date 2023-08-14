2cab368d3925c66dc902a52224ecf27a882cd760
Berrypie2706 <aniket.chouksey444@gmail.com>
2023-08-14 15:39:07 +0530

    Second commit

--- Assignment3.sql
+++ Assignment3.sql
@@ -30,7 +30,7 @@ insert into ProductInfo values(108, 'Earpod', 1000.90, '12/01/2022', 3)
 insert into ProductInfo values(109, 'Laptop', 85000.90, '12/12/2021', 6)
 insert into ProductInfo values(110, 'Mobile', 55000.70, '12/12/2020', 1)
 
-select p.PId, p.PName, p.PPrice, p.PMDate, c.CName
+select c.CName, p.PId, p.PName, p.PPrice, p.PMDate
 from ProductInfo p
 join CompanyInfo c on p.CId = c.CId
 
@@ -42,4 +42,47 @@ join CompanyInfo c on p.CId = c.CId
 select p.PId, p.PName, p.PPrice, p.PMDate, c.CName,c.CId
 from ProductInfo p
 cross join CompanyInfo c 
+---------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
+--Part2
+
+create table Products(
+PId int primary key,
+PQ int not null,
+PPrice float,
+Discount float )
+
+
+
+insert into Products values( 1, 24, 24000.89, 0.15)
+insert into Products values( 2, 24, 19000.89, 0.10)
+insert into Products values( 3, 24, 15000.89, 0.25)
+
+create function Discount
+(
+@price float,
+@discount float
+)
+returns float
+as 
+begin
+declare @discountGiven float;
+select @discountGiven = @price * @discount
+return @discountGiven
+end
+
+create function CalcDiscountValue
+(
+@price float,
+@discount float
+)
+returns float
+as 
+begin
+declare @discountValue float;
+select @discountValue= @price - (@price * @discount)
+return @discountValue
+end
+
+select PId, PQ, PPrice, Discount, dbo.Discount(PPrice,Discount) as 'Discount Given', dbo.CalcDiscountValue(PPrice,Discount) as 'Price After Discount' from Products
 
